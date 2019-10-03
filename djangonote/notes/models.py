ed26f440f1306ac4dceff44a8e08a10753f7858b
Akshat Zala <akshat.zala@cuelogic.com>
2019-10-03 16:46:07 +0530

    Multi User Feature

--- djangonote/notes/models.py
+++ djangonote/notes/models.py
@@ -2,14 +2,17 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from django.utils.text import slugify
-
+from django.utils import timezone
 
 class Note(models.Model):
 	label = models.CharField(max_length=200)
 	body = models.TextField()
 	timestamp = models.DateTimeField(auto_now_add=True)
+	user = 	models.ForeignKey(User,on_delete=models.CASCADE)
 	tags = models.ManyToManyField('Tag', related_name='notes', blank=True)
 
+	def __str__(self):
+		return self.label
 
 class Tag(models.Model):
 	label = models.CharField(max_length=200)
--- djangonote/users/models.py
+++ djangonote/users/models.py
@@ -6,3 +6,8 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     """docstring for Profile"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+    	user  = self.user
+    	return Profile.objects.filter(user=user)
