12ee68ecbecb82f8ac8f3c32ac51e88ca2916752
Akshat Zala <Akshat.Zala@cuelogic.com>
2019-10-11 16:25:22 +0530

    Field Type changed

--- djangonote/notes/models.py
+++ djangonote/notes/models.py
@@ -5,35 +5,33 @@ from django.utils.text import slugify
 
 
 class Tag(models.Model):
-	label = models.CharField(max_length=200)
-	slug = models.SlugField(max_length=200, unique=True)
+    label = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
 
-	def __str__(self):
-		return self.label
+    def __str__(self):
+        return self.label
 
-	def _get_unique_slug(self):
-		slug = slugify(self.label)
-		unique_slug = slug
-		num = 1
-		while Tag.objects.filter(slug=unique_slug).exists():
-			unique_slug = '{}-{}'.format(slug, num)
-			num += 1
-		return unique_slug
+    def _get_unique_slug(self):
+        slug = slugify(self.label)
+        unique_slug = slug
+        num = 1
+        while Tag.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+        return unique_slug
 
-	def save(self, *args, **kwargs):
-		if not self.slug:
-			self.slug = self._get_unique_slug()
-		super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+        super().save(*args, **kwargs)
 
 
 class Note(models.Model):
-	label = models.CharField(max_length=200)
-	body = models.TextField()
-	timestamp = models.DateTimeField(default=timezone.now)
-	tags = models.ForeignKey('Tag', related_name='notes', blank=True, on_delete=models.CASCADE, null=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+    label = models.CharField(max_length=200)
+    body = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+    tags = models.ForeignKey('Tag', related_name='notes', blank=True, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.label
-
-
+    def __str__(self):
+        return self.label
