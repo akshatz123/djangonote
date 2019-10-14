2ffd9c9d4d1a09cf2ca99dd3627013567c0897a5
Akshat Zala <akshat.zala@cuelogic.com>
2019-10-14 13:29:06 +0530

    URL Dispatcher

--- djangonote/notes/urls.py
+++ djangonote/notes/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('', index_view, name='notes.index_view'),
 	path('addnote/', add_note, name='notes.addnote'),
 	path('addtag/', add_tag, name='notes.addtag'),
-	url(r'^tags/(?P<slug>[-\w]+)/$', tag_search, name='notes.tagsearch'),
+	path('tags/<slug:slug>', tag_search, name='notes.tagsearch'),
 	path('search/', search, name='notes.search_results'),
 ]
